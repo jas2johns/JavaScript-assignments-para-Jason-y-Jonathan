156ce3d36d7a96aefa2c0e26ebda8eb9702b07fc
jas2johns <70867335+jas2johns@users.noreply.github.com>
2020-10-28 11:25:04 -0700

    Add files via upload

---
+++ JQuery_Asgn5.js
@@ -0,0 +1,17 @@
+// make 4 event handlers
+    
+// 1st hover on mouse over handler for the 3 images
+    //mouse hover over change on imge hovered over
+
+// event handler on mouse over/ hover to have embossed animation same as line 4
+
+//click event for each the imges the main 1's
+    // transmition animation for the big img
+
+//submit me button click event
+    // validate first name text box 
+    // display the message "result"
+
+// hide img click event
+    //hide the big img
+    // hide the result message 
