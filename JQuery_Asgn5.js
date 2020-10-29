9206567142dc73e0cbb063ed150582f715987430
Jonathan Carter <jon@placidbits.com>
2020-10-28 20:59:19 -0400

    Revert "1st event"
    This reverts commit 08abb5e8fba0d5437619ef985139932fa367b38d.

--- JQuery_Asgn5.js
+++ JQuery_Asgn5.js
@@ -1,30 +1,17 @@
-$(document).ready(function(){
-    // 1st hover on mouse over handler for the 3 images
-    $("img").hover(
-        function(event){
-            // mouse over
-            event.target.style.borderStyle = "outset";
-            event.target.style.borderWidth = "10px";
-            event.target.style.borderRightColor = "black";
-            event.target.style.borderBottomColor = "black";
-        },
-        function(event) {
-            // mouse out
-            event.target.style.border = "0px";
-        });
+// make 4 event handlers
+    
+// 1st hover on mouse over handler for the 3 images
+    //mouse hover over change on imge hovered over
 
-    // 2nd event handler on mouse over/ hover to have embossed animation same as line 4
+// event handler on mouse over/ hover to have embossed animation same as line 4
 
-    // 3rd click event for each the imges the main 1's
-        // transmition animation for the big img
+//click event for each the imges the main 1's
+    // transmition animation for the big img
 
-    // 4th submit me button click event
-        // validate first name text box 
-        // display the message "result"
+//submit me button click event
+    // validate first name text box 
+    // display the message "result"
 
-    // 5th hide img click event
-        //hide the big img
-        // hide the result message 
-
-
-});
+// hide img click event
+    //hide the big img
+    // hide the result message 
