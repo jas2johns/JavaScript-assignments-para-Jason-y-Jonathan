9ffe3fddd6fafa4c382f58c4bc51b14aa7eb3056
jas2johns <70867335+jas2johns@users.noreply.github.com>
2020-11-08 15:59:00 -0800

    Add files via upload

--- Assignment_6.js
+++ Assignment_6.js
@@ -1,7 +1,60 @@
-//document
+$(document).ready(function(){
+        $('#mysubmit').click(function(){
+      // step 1 validate first name input
 
-//make hidden div yellow dynamically!
 
-// focus() attribute should go to any required error fields!!
+    var firstName = $('#first_name').val();
+    if (firstName == '') {
+        $('#first_error').html('You must enter a first name');
+        $('#first_name').focus();
+        return;
+    }
 
-//
+    // stp 2 validate last name input
+           var lastName = $('#last_name').val();
+            if (lastName == '') {
+                $('#last_error').html('You must enter a last name');
+                $('#last_name').focus();
+            return;
+            }
+    // same process for gender
+         //   var gender = $('input [name="gender"]:checked').val();
+         //   if (!gender) {
+           //     $('#gender_error').html('You must select a gender');
+                
+                // this does not work!!! the focus attribute
+                
+                //$(!'input [name="gender"]: checked').focus();
+                
+                return;
+           // }
+    // same process for years of experience
+            var years = $('#years').val();
+            if (years == '-') {
+                $('#years_error').html("You must select a number");
+                $('#years').focus();
+                return;
+            } 
+            
+
+            var yearsInt == parseInt(years);
+            //var yellowBox ;
+            
+            // $(yellowBox).css( "yellow" , '300px' , '300px' );
+            
+    // made div innerhtml message appear
+           // var msg = $('#message').html("You are a " "You have: " + gender + <br> + yearsInt + "years experience ).css( "yellow" , '300px' , '300px' );
+            
+            
+            // im not sure if line 45 is Concatenated properly....   but the real issue is line 20 to 30
+            
+            
+    //make hidden div yellow dynamically!
+
+    
+                                         
+    //
+    });
+
+});
+
